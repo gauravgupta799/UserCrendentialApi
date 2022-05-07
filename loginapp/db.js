cc34f3aaba17ec5820a5ba49deac7a73a5195ce1
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-05-07 20:05:39 +0530

    userCredentialApi

---
+++ loginapp/db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/augintern')
+mongoose.connect("mongodb+srv://gauravcom:gupta123@cluster0.wzm9s.mongodb.net/zomato?retryWrites=true&w=majority")
